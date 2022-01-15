60510ea6537d16f859e4b21eb258ec3e56633d77
davidlao <61684228+davidlao27@users.noreply.github.com>
2022-01-15 20:29:39 +0100

    maybe today is the day

--- jsm/loaders/GLTFLoader.js
+++ jsm/loaders/GLTFLoader.js
@@ -62,7 +62,7 @@ import {
 	Vector3,
 	VectorKeyframeTrack,
 	sRGBEncoding
-} from './https://raw.githubusercontent.com/davidlao27/davidMR/main/three.module.js';
+} from '../../../build/three.module.js';
 
 class GLTFLoader extends Loader {
 
